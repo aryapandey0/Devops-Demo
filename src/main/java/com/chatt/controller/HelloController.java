db380a6a8a35a9bcecb01ab30e1a558582584452
Arya Pandey <aryapandey376@gmail.com>
2025-09-28 11:32:35 +0530

    done

--- src/main/java/com/chatt/controller/HelloController.java
+++ src/main/java/com/chatt/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.chatt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,10 @@ public class HelloController {
 	@GetMapping
 	public String hello() {
 		return "Hello From Docker";
+	}
+	@GetMapping("/name")
+	public String name(@RequestParam String name) {
+		return "Hello "+ name;
 	}
 	
 }

59cea2e8af4a15122ab63424e64325af4335850c
Arya Pandey <aryapandey376@gmail.com>
2025-09-28 06:40:44 +0530

    first commit

---
+++ src/test/java/com/chatt/AutomateHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomateHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
